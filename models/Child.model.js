e8ce5f8dd3594c9850b4e4c36f1d901b91366d5b
Sophie Krag <sophie.9@live.nl>
2021-07-31 13:28:32 +0200

    Create user, story and child model

---
+++ models/Child.model.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose")
+
+const childSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        firstname: {
+            type: String,
+            required: true,
+        },
+        lastname: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        creator: {
+            type: [{ type: Schema.Types.ObjectId, ref: "User"}],
+        },
+        stories: {
+            type: [{ type: Schema.Types.ObjectId, ref: "Story"}],
+        },
+        timestamp: true,
+    }
+)
+
+module.exports = model("Child", childSchema)
