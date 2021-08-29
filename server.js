15c127facd073013115203a631782b76568e67bc
Sophie Krag <sophie.9@live.nl>
2021-08-29 17:54:37 +0200

    Added cookieparser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookieparser")
 
 const app = express();
 
@@ -9,6 +10,7 @@ const app = express();
 require("./config/db.config");
 require("./config/session.config")(app);
 
+app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
